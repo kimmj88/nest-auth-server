171196ae00ada56ad2794328c2d333b8b8c83054
kimmj88 <daialop88@nate.com>
2024-08-30 10:52:01 +0900

    test push

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateUserDto } from 'src/user/user.dto';
-import { UserService } from 'src/user/user.service';
-import * as bcrypt from 'bcrypt';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { CreateUserDto } from "src/user/user.dto";
+import { UserService } from "src/user/user.service";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -10,7 +10,7 @@ export class AuthService {
   async register(userDto: CreateUserDto) {
     const user = await this.userService.getUser(userDto.email);
     if (user) {
-      throw new HttpException('alreay user account', HttpStatus.BAD_REQUEST);
+      throw new HttpException("alreay user account", HttpStatus.BAD_REQUEST);
     }
     const encryptedPassword = bcrypt.hashSync(userDto.password, 10);
 
@@ -22,7 +22,7 @@ export class AuthService {
       user.password = undefined;
       return user;
     } catch (error) {
-      throw new HttpException('Server Error', 500);
+      throw new HttpException("Server Error", 500);
     }
   }
 
